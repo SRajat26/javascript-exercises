072686474784320488f4fed35fc13c7ed4b29b5c
SRajat26 <shreyansh.rajat.26@gmail.com>
2025-02-02 21:30:21 +0530

    Completed 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  let x = (far - 32) * 5 / 9;
+  if(Number.isInteger(x)){
+  return x;}
+  else
+return Math.round(x*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let x = (cel * 9 / 5) + 32;
+  if(Number.isInteger(x)){
+    return x;
+  }
+  else
+  return Math.round(x*10)/10;
 };
+
 
 // Do not edit below this line
 module.exports = {

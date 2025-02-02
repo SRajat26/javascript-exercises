dc18bf1abdb9f1245fba82f94986d8bbb5ef884a
SRajat26 <shreyansh.rajat.26@gmail.com>
2025-02-02 21:14:14 +0530

    Completed till 6

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let x="";
+for(let i=word.length-1;i>=0;i--){
+    x= x+word[i];
+}
+return x;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...arg) {
+    let newarr=[];
+    for(item of arr){
+        if(arg.includes(item))
+            continue;
+        else
+            newarr.push(item);
+    }
+    return newarr;
 };
 
 // Do not edit below this line
